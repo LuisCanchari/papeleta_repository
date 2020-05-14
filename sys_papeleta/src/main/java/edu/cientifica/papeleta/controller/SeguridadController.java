7513ea7c0ca512afcbbff028b3866366b96ce69e
Computer <Computer@192.168.1.7>
2020-05-14 14:19:10 -0500

    segunda actualizacion

--- sys_papeleta/src/main/java/edu/cientifica/papeleta/controller/SeguridadController.java
+++ sys_papeleta/src/main/java/edu/cientifica/papeleta/controller/SeguridadController.java
@@ -1,13 +1,43 @@
 package edu.cientifica.papeleta.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+/*@Controller: anotacion para designar una clase Controlador del patron MVC*/
 @Controller
 public class SeguridadController {
+
+	/* @GetMapping: anotacion spring, asocia la peticion GET con un metodo */
 	@GetMapping("/")
 	public String login() {
+
 		return "login";
-	
+
+	}
+
+	/*
+	 * @RequestMapping: anotacion que asocia peticiones (GET o POST) con un metodo
+	 * 
+	 * @RequestParam: anotacion que lee los parametros de peticiones POST
+	 */
+	@RequestMapping(value = "/autentiacion", method = RequestMethod.POST)
+	public String autenticacion(Model model, @RequestParam(name = "usuario") String usuario,
+			@RequestParam(name = "password") String password) {
+		String mensaje;
+		if (usuario.equals("clave")) {
+			mensaje = "Password correcto";
+
+		} else {
+			mensaje = "Password incorrecto";
+		}
+
+		model.addAttribute("msg", mensaje);
+		model.addAttribute("username", mensaje);
+
+		return "resultado";
 	}
 }
